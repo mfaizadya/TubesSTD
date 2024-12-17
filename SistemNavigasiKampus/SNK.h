fad744082ab8d196aabe266287e7d7c4d4c5bdd2
Frizam <frizammaulana07@gmail.com>
2024-12-17 17:52:34 +0700

    Update SNK.h

--- SistemNavigasiKampus/SNK.h
+++ SistemNavigasiKampus/SNK.h
@@ -1,14 +1,21 @@
 #ifndef SNK_H_INCLUDED
 #define SNK_H_INCLUDED
 #include <iostream>
+
+#define start(P) P.start
+#define info(P) P->info
+#define firstJalan(P) P->firstJalan
+#define nextBuilding(P) P->nextBuilding
+#define nextJalan(P) P->nextJalan
+#define destination(P) P->destination
+
 using namespace std;
 
-//node = vertex = building
-//edge = bridge = jalan
+// node = vertex = building
+// edge = bridge = jalan
 
 struct infotypeJalan {
-    int jalan;
-    //jalan = weight
+    int weight; // jalan = weight
 };
 
 struct infotypeBuilding {
@@ -21,12 +28,13 @@ typedef struct elmtJalan *adrJalan;
 struct elmtBuilding {
     infotypeBuilding info;
     adrJalan firstJalan;
-    adrBuilding nextNode;
+    adrBuilding nextBuilding;
 };
 
 struct elmtJalan {
     infotypeJalan info;
-    adrJalan nextEdge;
+    adrJalan nextJalan;
+    adrBuilding destination; // destination building
 };
 
 struct Graph {
@@ -35,42 +43,39 @@ struct Graph {
 
 void createGraph_103022300048_103022300011(Graph &G); //Membuat graph
 /*{I.S. Belum terdefinisi sebuah graph
- F.S. Telah dibuat sebuah graph baru dengan start(G) = nil}*/
-void createRoute_103022300048_103022300011(adrJalan J); //Membuat rute
-/*{I.S. Belum terdefinisi sebuah rute (edge)
- F.S. Telah dialokasi sebuah rute baru dengan infoRoute(R) sesuai parameter,
-      nextEdge(R) = nil}*/
+ F.S. Telah dibuat sebuah graph baru dengan start(G) = NULL}*/
+
 adrBuilding allocateBuilding_103022300048_103022300011(infotypeBuilding infoBuilding); //alokasi elemen gedung
-/*{Mengembalikan nilai pointer ke sebuah elemen gedung yang baru dialokasikan
- dengan infoBuilding(v) = infoBuilding, firstEdge(v) = nil, dan nextNode(v) = nil}*/
-adrRoute allocateJalan_103022300048_103022300011(infotypeJalan info); //alokasi elemen rute
-/*{Mengembalikan nilai pointer ke sebuah elemen rute yang baru dialokasikan
- dengan infoRoute(R) = infoRoute dan nextEdge(R) = nil}*/
-void addBuilding_103022300048_103022300011(Graph &G, string B); //Menambah gedung
-/*{I.S. Mungkin ada atau tidak ada gedung di graph G
- F.S. Sebuah gedung baru dengan nama B ditambahkan ke graph G,
-      menjadi elemen terakhir dalam daftar building (nextNode) atau menjadi start(G) jika kosong}*/
-void addJalan_103022300048_103022300011(Graph &G, string J); //Menambah Rute
-/*{I.S. Mungkin ada atau tidak ada rute di graph G
- F.S. Sebuah rute baru dengan informasi R ditambahkan di antara gedung yang relevan di graph G}*/
-void connecting_103022300048_103022300011(Graph &G, string b1, string b2); //Mengconnect antar building
-/*{I.S. Dua gedung (b1 dan b2) sudah ada di graph tetapi tidak saling terhubung
- F.S. Dibuat hubungan (edge/rute) di antara gedung b1 dan b2}*/
-adrBuilding findBuilding_103022300048_103022300011(Graph G, infotypeBuilding B); //Searching gedung
-/*{Mengembalikan nilai pointer ke elemen gedung dengan nama B jika ditemukan
- atau NULL jika tidak ada gedung dengan nama tersebut di graph G}*/
-adrJalan findJalan_103022300048_103022300011(Graph G, infotypeJalan J); //Searching rute
+/*{Mengembalikan pointer ke elemen building yang baru dengan infoBuilding terisi, firstJalan = NULL, dan nextBuilding = NULL}*/
+
+adrJalan allocateJalan_103022300048_103022300011(infotypeJalan info, adrBuilding destination); //alokasi elemen rute
+/*{Mengembalikan pointer ke elemen jalan yang baru dengan info terisi, destination terisi, dan nextJalan = NULL}*/
+
+void addBuilding_103022300048_103022300011(Graph &G, infotypeBuilding infoBuilding); //Menambah gedung
+/*{I.S. Graph mungkin kosong atau sudah memiliki building
+ F.S. Building baru ditambahkan sebagai elemen terakhir pada graph}*/
+
+void addJalan_103022300048_103022300011(Graph &G, string fromBuilding, string toBuilding, int jarak); //Menambah Rute
+/*{I.S. Dua building dengan nama fromBuilding dan toBuilding ada dalam graph
+ F.S. Jalan baru ditambahkan dari fromBuilding ke toBuilding dengan jarak tertentu}*/
+
+adrBuilding findBuilding_103022300048_103022300011(Graph G, string buildingName); //Searching gedung
+/*{Mengembalikan pointer ke elemen building dengan nama buildingName jika ditemukan, nullptr jika tidak}*/
+
+//adrJalan findJalan_103022300048_103022300011(Graph G, infotypeJalan J); //Searching rute
 /*{Mengembalikan nilai pointer ke elemen rute dengan informasi R jika ditemukan
  atau NULL jika tidak ada rute dengan informasi tersebut di graph G}*/
-void deleteLastJalan_103022300048_103022300011(Graph &G, adrBuilding B, adrRoute &J);
-/*{I.S. Sebuah gedung B memiliki daftar rute, dan setidaknya ada satu rute di daftar
- F.S. Rute terakhir di daftar rute gedung B dihapus, dan pointer R mengarah ke rute yang dihapus}*/
-void deleteAfterJalan_103022300048_103022300011(Graph &G, adrBuilding B, adrRoute Prec, adrRoute &J);
-/*{I.S. Sebuah gedung B memiliki daftar rute, dan terdapat rute setelah rute tertentu Prec
- F.S. Rute setelah Prec dihapus, dan pointer R mengarah ke rute yang dihapus}*/
-void shortestJalan_103022300048_103022300011(Graph &G, string awal, string terakhir); //Mencari rute terpendek
-/*{I.S. Graph G telah didefinisikan, serta terdapat dua gedung (start dan end) yang valid
- F.S. Menemukan dan menampilkan rute terpendek antara gedung start dan end berdasarkan weight rute}*/
+
+void deleteLastJalan_103022300048_103022300011(Graph &G, adrBuilding B, adrJalan &J);
+/*{I.S. Building memiliki daftar jalan
+ F.S. Jalan terakhir pada daftar dihapus, pointer J mengarah ke jalan yang dihapus}*/
+
+void deleteAfterJalan_103022300048_103022300011(Graph &G, adrBuilding B, adrJalan Prec, adrJalan &J);
+/*{I.S. Prec adalah pointer ke jalan sebelum jalan yang akan dihapus
+ F.S. Jalan setelah Prec dihapus, pointer J mengarah ke jalan yang dihapus}*/
+
+adrJalan shortestJalan_103022300048_103022300011(Graph &G, string buildingName); //Mencari jalan terpendek
+/*{Mengembalikan pointer ke elemen jalan dengan jarak terpendek dari gedung awal}*/
 
 
 #endif // SNK_H_INCLUDED
