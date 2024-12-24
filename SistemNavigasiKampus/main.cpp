f0f2f65eff956adb67dd8d0fdefbfbe83469ff0e
Frizam <frizammaulana07@gmail.com>
2024-12-24 16:45:16 +0700

    Update main.cpp

--- SistemNavigasiKampus/main.cpp
+++ SistemNavigasiKampus/main.cpp
@@ -5,6 +5,111 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Graph G;
+    createGraph_103022300048_103022300011(G);
+    int pilihan;
+    string namaGedung, gedungAwal, gedungTujuan;
+    int jarak;
+
+    do {
+        displayMenu_103022300048_103022300011();
+        cin >> pilihan;
+        cin.ignore(); // Membersihkan buffer
+
+        switch (pilihan) {
+            case 1: {
+                cout << "\n-- Tambah Gedung Baru --" << endl;
+                cout << "Masukkan nama gedung: ";
+                cin >> namaGedung;
+
+                infotypeBuilding newBuilding;
+                newBuilding.buildingName = namaGedung;
+                addBuilding_103022300048_103022300011(G, newBuilding);
+                cout << "Gedung berhasil ditambahkan!" << endl;
+                break;
+            }
+
+            case 2: {
+                cout << "\n-- Tambah Rute/Jalan --" << endl;
+                cout << "Gedung asal: ";
+                cin >> gedungAwal;
+                cout << "Gedung tujuan: ";
+                cin >> gedungTujuan;
+                cout << "Jarak (dalam meter): ";
+                cin >> jarak;
+
+                addJalan_103022300048_103022300011(G, gedungAwal, gedungTujuan, jarak);
+                cout << "Rute berhasil ditambahkan!" << endl;
+                break;
+            }
+
+            case 3: {
+                cout << "\n-- Cari Gedung --" << endl;
+                cout << "Masukkan nama gedung: ";
+                cin >> namaGedung;
+
+                adrBuilding found = findBuilding_103022300048_103022300011(G, namaGedung);
+                if (found != NULL) {
+                    cout << "Gedung '" << namaGedung << "' ditemukan!" << endl;
+
+                    // Tampilkan rute yang tersedia dari gedung ini
+                    cout << "Rute yang tersedia dari gedung ini:" << endl;
+                    adrJalan J = firstJalan(found);
+                    if (J == NULL) {
+                        cout << "- Tidak ada rute yang tersedia" << endl;
+                    } else {
+                        while (J != NULL) {
+                            cout << "- Ke " << info(destination(J)).buildingName
+                                 << " (Jarak: " << info(J).jarak << " meter)" << endl;
+                            J = nextJalan(J);
+                        }
+                    }
+                } else {
+                    cout << "Gedung tidak ditemukan." << endl;
+                }
+                break;
+            }
+
+            case 4: {
+                cout << "\n-- Gedung yang Sering Dikunjungi --" << endl;
+                frequentlyVisited_103022300048_103022300011(G);
+                break;
+            }
+
+            case 5: {
+                cout << "\n-- Petunjuk Arah --" << endl;
+                cout << "Gedung asal: ";
+                cin >> gedungAwal;
+                cout << "Gedung tujuan: ";
+                cin >> gedungTujuan;
+
+                showRouteSteps_103022300048_103022300011(G, gedungAwal, gedungTujuan);
+                break;
+            }
+
+            case 6: {
+                cout << "\n-- Cari Rute Terpendek --" << endl;
+                cout << "Gedung asal: ";
+                cin >> gedungAwal;
+                cout << "Gedung tujuan: ";
+                cin >> gedungTujuan;
+
+                findShortestRoute(G, gedungAwal, gedungTujuan);
+                break;
+            }
+
+            case 0: {
+                cout << "\nTerima kasih telah menggunakan Sistem Navigasi Kampus!" << endl;
+                break;
+            }
+
+            default: {
+                cout << "Pilihan tidak valid!" << endl;
+                break;
+            }
+        }
+
+    } while (pilihan != 0);
+
     return 0;
 }
